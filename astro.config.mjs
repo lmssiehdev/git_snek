d9f8305218a7e3fe2f7f2c239eaaa69cfc50af83
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-02-13 21:59:30 +0100

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,16 @@ import solidJs from "@astrojs/solid-js";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [solidJs()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
